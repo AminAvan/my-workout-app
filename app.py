25eb58b7d16f2a9655b51644dba2cd81e55a8c1e
Amin <aminavan.hardware@yahoo.com>
2023-02-26 21:24:47 -0500

    change14 line 160 decode utf 8

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def user_signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         new_user = usercredentials(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
